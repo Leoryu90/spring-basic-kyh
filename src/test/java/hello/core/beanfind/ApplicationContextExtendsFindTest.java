929339133c41cbb1d377dcf94bb49559dab9054f
Seokho ryu <thetein@gmail.com>
2024-11-28 22:38:20 +0900

    20241128 kyh

--- src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -1,10 +1,33 @@
 package hello.core.beanfind;
 
+import java.util.concurrent.ThreadPoolExecutor.DiscardPolicy;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import hello.core.AppConfig;
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 
 public class ApplicationContextExtendsFindTest {
 
-	AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+	AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+	
+	
+	
+	@Configuration
+	static class TestConfig{
+		
+		@Bean
+		public DiscountPolicy reteDiscountPolicy() {
+			return new RateDiscountPolicy();
+		}
+		
+		@Bean
+		public DiscountPolicy fixDiscountPolicy() {
+			return new FixDiscountPolicy();
+		}
+	}
 }
